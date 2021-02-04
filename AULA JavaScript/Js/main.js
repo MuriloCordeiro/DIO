31dd6e21b9f1047daf9bdcfa24ac58c4a95113b1
murilo <murilo@dash.com>
2021-02-04 14:03:03 -0300

    Netflix clone

---
+++ AULA JavaScript/Js/main.js
@@ -0,0 +1,48 @@
+
+let line = gets().split(" ");
+let A = parseInt(line[0]);
+let B = parseInt(line[1]);
+let total = 0; // Altere o valor da variável com o cálculo esperado
+console.log("X = " + total);
+
+/*var count = 0;
+while (count <= 5){
+    console.log(count);
+    count = count + 1;
+}
+
+
+/*
+var idade = prompt("Qual sua idade");
+var idade = 18;
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};*/
+
+/*var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+
+
+//var lista = ["Maça", "pêra", "laranja"]
+//lista.push("uva")
+
+
+//lista.pop();
+//console.log(lista.reverse);
+
+
+
+//var nome = "Murilo Cordeiro";
+//var idade = 23;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));*/
+ 
